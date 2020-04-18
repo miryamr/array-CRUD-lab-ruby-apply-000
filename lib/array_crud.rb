14246fb3bd49762efe4cfbd74a9f604b6dfd323b
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-18 06:31:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[]
+
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[]
+  array[5]
 end
